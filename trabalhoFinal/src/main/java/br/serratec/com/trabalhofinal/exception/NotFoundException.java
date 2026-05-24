2fe5ca66eab7846f8c8a7c7182298dc45273b7c4
llcardos <137669141+llcardos@users.noreply.github.com>
2026-05-24 16:17:51 -0300

    feat: package exception

---
+++ trabalhoFinal/src/main/java/br/serratec/com/trabalhofinal/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package br.serratec.com.trabalhofinal.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+    
+
+}
