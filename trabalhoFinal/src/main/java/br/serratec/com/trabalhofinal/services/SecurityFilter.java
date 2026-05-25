8d0b7b36a6e08f6567d36a854565bb68b24b48f8
Kevin <kevinrezende55@outlook.com>
2026-05-25 16:33:20 -0300

    Adicionando JWT ainda em desenvolvimento no tokem

---
+++ trabalhoFinal/src/main/java/br/serratec/com/trabalhofinal/services/SecurityFilter.java
@@ -0,0 +1,49 @@
+package br.serratec.com.trabalhofinal.services;
+
+import br.serratec.com.trabalhofinal.repository.ClienteRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recuperarToken(request);
+
+        if (token != null) {
+            String subject = JWT.require(Algorithm.HMAC256("minha-chave-secreta-super-protegida"))
+                    .withIssuer("api-mecanica").build().verify(token).getSubject();
+
+            UserDetails usuario = (UserDetails) repository.findAll(subject).orElseThrow();
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
